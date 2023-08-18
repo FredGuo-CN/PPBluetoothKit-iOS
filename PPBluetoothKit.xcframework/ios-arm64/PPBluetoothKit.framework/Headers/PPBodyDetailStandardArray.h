d95c58c4836d59f8370821db64b4f16f90af3c88
sahooz <2423096504@qq.com>
2023-08-18 19:10:16 +0800

    Initial

---
+++ PPBluetoothKit.xcframework/ios-arm64/PPBluetoothKit.framework/Headers/PPBodyDetailStandardArray.h
@@ -0,0 +1,77 @@
+//
+//  PPBodyDetailStandardArray.h
+//  PPBluetoothKit
+//
+//  Created by 彭思远 on 2023/6/20.
+//
+
+#import <Foundation/Foundation.h>
+#import "PPBodyFatModel.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+
+typedef NS_ENUM(NSUInteger, BodyDetailUnitType) {
+    BodyDetailUnitTypeValue,
+    BodyDetailUnitTypePercent,
+    BodyDetailUnitTypeWeight,
+};
+
+@interface PPBodyDetailStandardArray : NSObject
+
+
+- initWithHeightCm:(NSInteger)heightCm
+            gender:(PPDeviceGenderType)gender
+               age:(NSInteger)age
+            weight:(CGFloat)weight;
+
++ (NSInteger)calculateCurrentStandardWithValue:(CGFloat)number andStandardArray:(NSArray *)array;
+
+- (NSArray *)sortValuesFromDictionary:(NSDictionary *)dictionary andType:(BodyDetailUnitType)type;
+
+- (NSArray *)fetchPPBodyParam_Weight_StandartArray;
+
+- (NSArray *)fetchPPBodyParam_BodyFat_StandartArray;
+
+- (NSArray *)fetchPPBodyParam_Water_StandartArray:(NSDictionary *)dic;
+
+- (NSArray *)fetchPPBodyParam_Water_StandartArray;
+
+- (NSArray *)fetchPPBodyParam_Mus_StandartArray:(NSDictionary *)dic;
+
+- (NSArray *)fetchPPBodyParam_Mus_StandartArray;
+
+- (NSArray *)fetchPPBodyParam_Bone_StandartArray:(NSDictionary *)dic;
+
+- (NSArray *)fetchPPBodyParam_Bone_StandartArray;
+
+- (NSArray *)fetchPPBodyParam_BMR_StandartArray;
+
+- (NSArray *)fetchPPBodyParam_BMI_StandartArray8;
+
+- (NSArray *)fetchPPBodyParam_BMI_StandartArray;
+
+- (NSArray *)fetchPPBodyParam_VisFat_StandartArray;
+
+- (NSArray *)fetchPPBodyParam_proteinPercentage_StandartArray;
+
+- (NSArray *)fetchPPBodyParam_BodySubcutaneousFat_StandartArray;
+
+- (NSArray *)fetchPPBodyParam_BodySkeletal_StandartArray;
+
+- (NSArray *)fetchPPBodyParam_heart_StandartArray;
+
+- (NSArray *)fetchPPBodyParam_BodyHealth_StandartArray;
+
+- (NSArray *)fetchPPBodyParam_FatGrade_StandartArray;
+
+- (NSArray *)fetchPPBodyParam_physicalAgeValue_StandartArray;
+
+- (NSArray *)fetchPPBodyParam_BodyScore_StandartArray;
+
+- (NSArray *)percent2kg:(NSArray *)source;
+
+- (NSArray *)kg2percent:(NSArray *)source;
+@end
+
+NS_ASSUME_NONNULL_END
